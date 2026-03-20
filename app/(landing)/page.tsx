5be242a45cbb42f343fc9b74bdfc21ec5178c3dd
Soulgnadeth <toulpc9@gmail.com>
2026-03-20 18:50:04 +0700

    feat: initial commit

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  const name = "Tou";
+  return (
+    <>
+      <h1 className="text-4xl text-amber-600 font-bold animate-bounce">Hello, World!</h1>
+      <p>Welcome {name} to my Next.js app.</p>
+    </>
+  );
+}
